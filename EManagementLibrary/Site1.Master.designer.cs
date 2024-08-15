dd471232af1a554befb59a631822117872b253f5
DzomboAllan <adzombo@seaburysolutions.com>
2024-08-15 16:06:20 +0300

    Feat:User Login and Home  Page Content

--- EManagementLibrary/Site1.Master.designer.cs
+++ EManagementLibrary/Site1.Master.designer.cs
@@ -78,13 +78,13 @@ namespace EManagementLibrary
         protected global::System.Web.UI.WebControls.LinkButton LinkButton7;
 
         /// <summary>
-        /// ContentPlaceHolder control.
+        /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
 
         /// <summary>
         /// LinkButton6 control.
